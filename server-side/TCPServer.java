98e34dd525bb5649a9df1459c070c7674452cf30
Andrei Ghenoiu <andrei_stefang@yahoo.com>
2012-08-23 12:22:51 -0400

    server-side files commit

---
+++ server-side/TCPServer.java
@@ -0,0 +1,110 @@
+import java.io.*;
+import java.net.*;
+ public class TCPServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		String clientSentence;
+		String serverSentence;
+		ServerSocket welcomeSocket;
+		Socket serverSocket;
+		
+		String miss = "miss";
+		String hit = "hit";
+		String won = "won";
+		boolean turn = true;
+		
+		
+		//from command line
+		BufferedReader inFromUser = new BufferedReader(
+				new InputStreamReader(System.in));
+		//create the board
+		
+		board gameBoard = new board();
+		
+		//player is asked to add the ships to the board
+		System.out.println("Below you can input where you want to" +
+				" place your battleships.\n Please enter them in integers" +
+				" starting with the row followed by columns\n (for example" +
+				" start with the head as 11 for row 1,\n column 1 and " +
+				" tail as 51 for row 5 and column 1)\n. Please input the data\n " +
+				"left to right and top to bottom\n" +
+				"Type q when done.");
+		while(true){
+			System.out.println("Please enter head location:");
+			String line1 = inFromUser.readLine();
+			System.out.println("Please enter tail location:");
+			String line2 = inFromUser.readLine();
+			if(!line1.equals("q") || !line2.equals("q")){
+				int head = Integer.parseInt(line1);
+				int tail = Integer.parseInt(line2);
+				//we call the testPos method to verify that we can place
+				//the battleship at the inputed locations
+				gameBoard.testPos(head, tail);
+				if(gameBoard.boatBool == true){
+					gameBoard.createBoat(head, tail);
+					System.out.println("Creating boat at "+head+ " and "+tail);
+				}
+				else
+					System.out.println("Sorry, can't place the battleship using these locations.");
+			}
+			else
+				break;
+		}
+		
+		gameBoard.printBoard();
+		////////////////////////////////////
+		//game starts
+		////////////////////////////////////
+		welcomeSocket = new ServerSocket(6789);
+		serverSocket  = welcomeSocket.accept();
+		//from client
+		BufferedReader inFromClient = new BufferedReader(
+				new InputStreamReader(serverSocket.getInputStream()));
+		
+		//out to client the hit or miss message
+		DataOutputStream outToClient1 = 
+			new DataOutputStream(serverSocket.getOutputStream());
+		
+		
+		while(true){ 
+			int hitRow;
+			int hitCol;
+			
+			//the server receives a hit from the client 
+			//and replies with a hit or miss
+			clientSentence = inFromClient.readLine();
+			System.out.println("Hit from client: " + clientSentence);
+			int clientInt = Integer.parseInt(clientSentence);
+			hitRow = Math.abs(clientInt/10)-1;
+			hitCol = clientInt%10-1;
+			
+			if(gameBoard.testHit(hitRow, hitCol)){
+				gameBoard.testLoss();
+				if(gameBoard.testLoss() == false){
+					hit = miss = "You won!";
+					System.out.println("Sorry, you lost!");
+				}
+				outToClient1.writeBytes(hit+"\n");
+				outToClient1.flush();
+			}
+			else{
+				outToClient1.writeBytes(miss+"\n");
+				outToClient1.flush();
+			}
+			
+			//the server sends a hit
+			String newS = inFromUser.readLine();
+			outToClient1.writeBytes(newS+"\n");
+			outToClient1.flush();
+			
+			clientSentence = inFromClient.readLine();
+			System.out.println("Result from client: " + clientSentence);
+			
+		}
+	}
+
+}
---
+++ server-side/board.java
@@ -0,0 +1,84 @@
+
+
+
+public class board {
+	public int[][] board;
+	//this will hold info if the boat can be created
+	public boolean boatBool;
+	public boolean loss = true;
+	public String  str;
+	public board(){
+		board = new int[10][10];
+	}
+	public void testPos(int a, int b){
+		int row1 = Math.abs(a/10);
+		int col1 = a%10;
+		int row2 = Math.abs(b/10);
+		int col2 = b%10;
+		if(row1 == row2 || col1 == col2){
+			boatBool = true;
+		}
+		else
+			boatBool = false;
+	}
+	//we create a boat
+	public void createBoat(int a, int b){
+		//we also need to test for the existence of other boats
+		
+		//create the head and tail of the ship
+		int row1 = Math.abs(a/10);
+		int col1 = a%10;
+		int row2 = Math.abs(b/10);
+		int col2 = b%10;
+		//testing to see if they are on the same column or row
+		//this way we know how to put the ship in the array
+		if(row1 == row2){
+			//if the boat is on the same row we have to fill the 
+			//columns out
+			for(int i = col1; i<=col2; i++){
+				board[row1-1][i-1] = 1;
+			}
+		}
+		else if(col1 == col2){
+			for(int i = row1; i<=row2; i++){
+				board[i-1][col1-1] = 1;
+			}
+		}
+		else{
+			boatBool = false;
+		}
+	}
+	//printing the board
+	public void printBoard(){
+		for(int i = 0; i<board.length; i++){
+			for(int c = 0; c<board[i].length; c++){
+				System.out.print(board[i][c]+" ");
+			}
+			System.out.println();
+		}
+	}
+	public boolean testHit(int row, int col){
+		if(board[row][col] == 1){
+			board[row][col] = 5;
+			printBoard();
+			return true;
+		}
+		else 
+			return false;
+	}
+	//test for loss, when all the board is 0's again
+	public boolean testLoss(){
+		int count = 0;
+		boolean bool1 = true;
+		for(int i = 0; i<board.length; i++){
+			for(int c = 0; c<board[i].length; c++){
+				if(board[i][c]==0 || board[i][c]==5){
+					count++;
+				}				
+			}
+		}
+		if(count == 100)
+			bool1 = false;
+		return bool1;
+	}
+}
